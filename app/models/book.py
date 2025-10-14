19dbda5508faf7127c10055dd02ef01a5cf5d16c
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-14 12:07:46 -0400

    Completes code through "Read All Books Endpoint" learn lesson

---
+++ app/models/book.py
@@ -0,0 +1,13 @@
+# Goal: client sends a request to get all existing books so they can see a list of books with their id, title, and description.
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
