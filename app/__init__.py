e38fb752b51931d5060e6e2e25d7d0f3d1648e80
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-28 11:00:45 -0400

    updates create_app function through test setup (6)

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from .db import db, migrate
 from .models import book
-# from .routes.hello_world_routes import hello_world_bp
 from .routes.book_routes import books_bp
+import os
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    if config:
+        app.config.update(config)
+    
 
     db.init_app(app)
     migrate.init_app(app, db)
+    
     # Register Blueprints here
-    # app.register_blueprint(hello_world_bp)
     app.register_blueprint(books_bp)
 
     return app
