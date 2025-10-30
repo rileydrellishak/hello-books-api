a1b9d73be71856b4cdb8a4eb19ec3f973483e4dd
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-30 11:50:18 -0400

    renames books_bp as bp and changes import statement to import bp as books_bp

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -2,9 +2,9 @@ from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
 
-books_bp = Blueprint('books_bp', __name__, url_prefix='/books')
+bp = Blueprint('bp', __name__, url_prefix='/books')
 
-@books_bp.post('')
+@bp.post('')
 def create_book():
     request_body = request.get_json()
     
@@ -20,7 +20,7 @@ def create_book():
 
     return new_book.to_dict(), 201
 
-@books_bp.get('')
+@bp.get('')
 def get_all_books():
     query = db.select(Book)
 
@@ -42,7 +42,7 @@ def get_all_books():
         books_response.append(book.to_dict())
     return books_response
 
-@books_bp.get('/<book_id>')
+@bp.get('/<book_id>')
 def get_one_book(book_id):
     book = validate_book(book_id)
     return book.to_dict()
@@ -63,7 +63,7 @@ def validate_book(book_id):
     
     return book
 
-@books_bp.put('/<book_id>')
+@bp.put('/<book_id>')
 def update_book(book_id):
     book = validate_book(book_id)
     request_body = request.get_json()
@@ -74,7 +74,7 @@ def update_book(book_id):
 
     return Response(status=204, mimetype='applications/json')
 
-@books_bp.delete('/<book_id>')
+@bp.delete('/<book_id>')
 def delete_book(book_id):
     book = validate_book(book_id)
 
@@ -85,7 +85,7 @@ def delete_book(book_id):
 
 # WAVES 1-2
 # from app.models.book import books
-# @books_bp.get('')
+# @bp.get('')
 # def get_all_books():
 #     books_response = []
 #     for book in books:
@@ -98,7 +98,7 @@ def delete_book(book_id):
 #         )
 #     return books_response
 
-# @books_bp.get('/<book_id>')
+# @bp.get('/<book_id>')
 # def get_one_book(book_id):
 #     book = validate_book(book_id)
     
