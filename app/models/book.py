311ef3acc8a703f3c42ad2661e054a3ee9ac39c7
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-30 15:40:20 -0400

    creates author model, establishes one to many for one author to many books

--- app/models/book.py
+++ app/models/book.py
@@ -1,4 +1,6 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
 
 class Book(db.Model):
@@ -6,6 +8,9 @@ class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+    author: Mapped[Optional["Author"]] = relationship(back_populates="books")
+
 
     @classmethod
     def from_dict(cls, book_data):
