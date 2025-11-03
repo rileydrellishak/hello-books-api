415212b93b4da624f8f5a6ddbdc17fef92d964db
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-02 19:42:44 -0500

    Implements create model route refactor

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -2,25 +2,14 @@ from flask import Blueprint, abort, make_response, request, Response
 from app.models.author import Author
 from app.models.book import Book
 from ..db import db
-from .route_utilities import validate_model
+from .route_utilities import validate_model, create_model
 
 bp = Blueprint('author_bp', __name__, url_prefix='/authors')
 
 @bp.post("")
 def create_author():
     request_body = request.get_json()
-
-    try:
-        new_author = Author.from_dict(request_body)
-        
-    except KeyError as error:
-        response = {"message": f"Invalid request: missing {error.args[0]}"}
-        abort(make_response(response, 400))
-    
-    db.session.add(new_author)
-    db.session.commit()
-
-    return make_response(new_author.to_dict(), 201)
+    return create_model(Author, request_body)
 
 @bp.get('/<author_id>')
 def get_author_by_id(author_id):
@@ -52,18 +41,8 @@ def create_book_with_author(author_id):
     author = validate_model(Author, author_id)
     request_body = request.get_json()
     request_body['author_id'] = author.id
-
-    try:
-        new_book = Book.from_dict(request_body)
     
-    except KeyError as error:
-        response = {'message': f'Invalid request: mising {error.args[0]}'}
-        abort(make_response(response, 400))
-
-    db.session.add(new_book)
-    db.session.commit()
-
-    return make_response(new_book.to_dict(), 201)
+    return create_model(Book, request_body)
 
 @bp.put('/<author_id>')
 def update_author(author_id):
